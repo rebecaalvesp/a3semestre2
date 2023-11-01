2bc9b3fecfb2ab6a356957113aecd14a59955d1c
rebecaalvesp <128102355+rebecaalvesp@users.noreply.github.com>
2023-10-31 21:15:40 -0300

    Create adicionar.php

---
+++ adicionar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Segurança</title>
+</head>
+<body>
+    <center>
+        <form id= "form-add"
+        action= "#" method="POST">
+        Nome: <input type="text"
+         name= "nome"><br>
+         CPF: <input type="text"
+         name="cpf"><br>
+         Telefone: <input type="text" name="telefone"><br>
+         Login: <input type="text" name= "login"><br>
+         Senha: <input type="password" name="senha"><br>
+         <input type="submit" name="cadastrar" value="Cadastrar">
+</form>
+</center>
+
+    
+</body>
+</html>
